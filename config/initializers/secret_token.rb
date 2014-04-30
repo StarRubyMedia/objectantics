39555b83afec071012cfab6be64e6b9e64acfe70
James Beine <james@objectantics.com>
2014-04-30 12:53:02 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Objectantics::Application.config.secret_key_base = '4b295a7ecb3b56b4e207fb5b49b7a1db5ee4f3ef454325e74ce20a14d128325817fabcbe9512def1eb7a13e08b700747c0af60af484b76c1fda2b0cd6f94586e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Objectantics::Application.config.session_store :cookie_store, key: '_objectantics_session'
